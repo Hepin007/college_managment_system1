cdbc9ff3dbb19f6079875fdd76cf0214ae115604
Hepin007 <159800392+Hepin007@users.noreply.github.com>
2025-06-19 17:03:02 +0530

    dsc

--- college_managment_system/college_managment_system/wsgi.py
+++ college_managment_system/college_managment_system/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_managment_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_managment_system.college_managment_system.settings')
 
 application = get_wsgi_application()
